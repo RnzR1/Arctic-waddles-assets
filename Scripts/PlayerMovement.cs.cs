40f29575e082d899bf2264523ff6565b9657fbb6
rinzeroufs <roufsrinze@gmail.com>
2024-04-09 12:34:23 +0200

    Initial commit

---
+++ Scripts/PlayerMovement.cs.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+ 
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+    
+    private void Awake()
+    {   
+        //Grabs references for rigidbody and animator from game object.
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+ 
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+ 
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+ 
+ 
+
+    }
+    
+}

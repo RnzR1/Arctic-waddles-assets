0b6e5c781660fc4f7c9abe1f1f755d374cd969b2
rinzeroufs <roufsrinze@gmail.com>
2024-04-12 20:29:55 +0200

    C:\Users\roufs\AppData\Roaming\UnityHub\Platformer\Assets

--- Scripts/PlayerMovement.cs.cs
+++ Scripts/PlayerMovement.cs.cs
@@ -24,14 +24,14 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
- 
+        
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2 (body.velocity.x, speed);
+
+        //Set animatotr parameters
+        anim.SetBool("run", horizontalInput != 0);
  
 
     }
-     private void Jump()
-    {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        anim.SetTrigger("jump");
-        grounded = false;
-    }
+   
 }
